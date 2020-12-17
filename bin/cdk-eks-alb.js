55b48ce426699d966bb10b2cd0e379984f495375
EC2 Default User <ec2-user@ip-172-31-29-201.us-east-2.compute.internal>
2020-12-17 00:07:15 +0000

    custom resource failed to create due to manifest too large

--- bin/cdk-eks-alb.js
+++ bin/cdk-eks-alb.js
@@ -4,4 +4,6 @@ const cdk = require('@aws-cdk/core');
 const { CdkEksAlbStack } = require('../lib/cdk-eks-alb-stack');
 
 const app = new cdk.App();
-new CdkEksAlbStack(app, 'CdkEksAlbStack');
+
+const _env = { account: '457175632986', region: 'ca-central-1' };
+new CdkEksAlbStack(app, 'CdkEksAlbStack', { env: _env });
